454233805170d98b0aacc6158f19508a022f77d8
suruchi203 <suruchijadhav29@gmail.com>
2019-03-15 07:17:43 +0000

    Add files via upload
    5x2 CV click prediction dataset, Naive Bayes

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ from weka.core.classes import Random
 import weka.plot.classifiers as plcls  # NB: matplotlib is required
 import os
 
-data_dir="/home/desais/BTech Project/"
+data_dir="/home/suruchi/Desktop/BTECH Pro/new/click_prediction/"
 
 jvm.start(packages=True)
 from weka.core.converters import Loader
 loader = Loader(classname="weka.core.converters.ArffLoader")
-data = loader.load_file(data_dir + "sonar.arff")
+data = loader.load_file(data_dir + "click_prediction.arff")
 data.class_is_last()
 
 #print(data)
@@ -19,10 +19,10 @@ data.class_is_last()
 cls = Classifier(classname="weka.classifiers.bayes.NaiveBayes")
 
 evl = Evaluation(data)
-evl.crossvalidate_model(cls, data, 10, Random(1))
+evl.crossvalidate_model(cls, data, 2, Random(5))
 
-print(evl.summary("=== NaiveBayes on sonar (stats) ===",False))
-print(evl.matrix("=== NaiveBayes on sonar(confusion matrix) ==="))
+print(evl.summary("=== NaiveBayes on click prediction (stats) ===",False))
+print(evl.matrix("=== NaiveBayes on click prediction(confusion matrix) ==="))
 #plcls.plot_classifier_errors(evl.predictions, absolute=False,wait = True)
 plcls.plot_roc(evl, class_index=[0,1], wait=True)
 print("areaUnderROC/1: " + str(evl.area_under_roc(1)))
