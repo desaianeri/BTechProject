1e00e60f5f5185ce43c77f0aa906daa85fca8d9c
Aneri Desai <desaianeri650@gmail.com>
2019-03-29 01:46:13 +0530

    Generalized code for multiple datasets working on 10cv

--- ui.py
+++ ui.py
@@ -1,8 +1,20 @@
 import Tkinter as tk
 from PIL import ImageTk, Image
 import tkFileDialog
-#from Tkinter import *
 from tkFileDialog   import askopenfilename
+import weka.core.jvm as jvm
+import weka.core.converters as conv
+from weka.classifiers import Evaluation, Classifier
+from weka.core.classes import Random
+import weka.plot.classifiers as plcls  # NB: matplotlib is required
+import os
+from weka.core.converters import Loader
+
+
+#Declaration of global variables
+
+final_algo = []
+final_data_list = []
 
 #Fullscreen window
 
@@ -11,6 +23,7 @@ root.title("ART")
 root.attributes('-zoomed', True)
 
 #generic code to center a window
+
 def center(win):
         """
         centers a tkinter window
@@ -31,7 +44,71 @@ def center(win):
 
 n = 100
 
+#RUN action
+
+def execute():
+
+	total_num_datasets = 0
+	dataset_string = []
+	print(final_algo)
+	print(len(final_algo))
+
+	#iterate over final_data_list to get total number of selected datasets
+	#i is the list
+
+	for i in final_data_list:
+		total_num_datasets = total_num_datasets + len(i)
+		for j in i:
+			dataset_string.append(j)
+
+	d = 0
+	jvm.start(packages = True)		#Must start and stop jvm once .Else runtime error of cannot start jvm
+	while(d < len(dataset_string)):
+
+		a = 0
+
+		while(a < len(final_algo)):
+
+			CV10(str(dataset_string[d]), str(final_algo[a]))	
+			a = a + 1
+
+		d = d + 1
+	jvm.stop()
+
+
+#Performs 10cv cross validation and stores the mean in the matrix
+
+def CV10(dataset,  algo):
+	print "inside 10cv"
+	print("dataset ----" + dataset)
+	print("algorithm ----" + algo)
+
+	#Executing 10FCV
+
+#	jvm.start(packages=True)
+	loader = Loader(classname="weka.core.converters.ArffLoader")
+	data = loader.load_file(dataset)
+	data.class_is_last()
+
+	#print(data)
+
+	cls = Classifier(classname="weka.classifiers.bayes.NaiveBayes")
+
+	evl = Evaluation(data)
+	evl.crossvalidate_model(cls, data, 2, Random(5))
+
+	print(evl.summary("=== NaiveBayes on click prediction (stats) ===",False))
+	print(evl.matrix("=== NaiveBayes on click prediction(confusion matrix) ==="))
+	#plcls.plot_classifier_errors(evl.predictions, absolute=False,wait = True)
+#	plcls.plot_roc(evl, class_index=[0,1], wait=True)
+	print("areaUnderROC/1: " + str(evl.area_under_roc(1)))
+
+#	jvm.stop()
+
+#Adding Datasets
+
 def filechoose():
+
     global n
     filez = tkFileDialog.askopenfilenames(parent=root,title='Choose a file')
     pathlabel2 = tk.Label(root)
@@ -40,23 +117,14 @@ def filechoose():
     m = 650
     pathlabel2.place(x = m ,y = n)
     n = n + 100
-    print root.tk.splitlist(filez)
+    dataset_list = root.tk.splitlist(filez)
+    final_data_list.append(dataset_list)
+#    print(final_data_list)
 
 
 
-def callback():
-	master = tk.Toplevel(root)
-	center(master)
-	width = master.winfo_screenwidth()
-	height = master.winfo_screenheight()
-	master.geometry('%sx%s' % (width/4, height/4))
-
-	d1 = tk.Button(master, text="Add dataset", width = 10, height = 2, bg = "blue", command=filechoose).grid(row = 0, sticky = tk.W)
-
-
-
+#Select algo
 def selectalgo():
-        print "click!"
         master = tk.Toplevel(root)
         center(master)
         width = master.winfo_screenwidth()
@@ -67,9 +135,9 @@ def selectalgo():
 
                 algo_list = [var1.get(), var2.get(), var3.get(), var4.get(), var5.get()]
                 l = len(algo_list)
-                print (algo_list)
+                print (algo_list)		
 
-                final_algo = []
+                
                 n = i = 0
 
 
@@ -92,8 +160,12 @@ def selectalgo():
                         n += 1
 
 
-                print(final_algo)
+                print(final_algo)	#Print algo list
+		algo_len = len(final_algo)
+		print ("Number of algorithms selected : " + str(algo_len))
                 master.destroy()
+
+
 
 
         label = tk.Label(master, text="Algorithms:", bg = "white").grid(row = 0, sticky = tk.W)
@@ -123,18 +195,18 @@ background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 dataset = tk.Button(root, text="Choose Datasets")
 photo = ImageTk.PhotoImage(Image.open("dataset.png"))
-dataset.config(image=photo,width ="130",height = "70", activebackground="black", bd=0, command = callback)
+dataset.config(image=photo,width ="130",height = "70", activebackground="black", bd=0, command = filechoose)
 
-algo = tk.Button(root, text="Choose Algorithms")
+algo = tk.Button(root, text="Choose Algorithms", foreground = 'red')
 photo1 = ImageTk.PhotoImage(Image.open("algo.png"))
 algo.config(image=photo1,width ="140",height = "60", activebackground="black", bd=0, command = selectalgo)
 
 run = tk.Button(root, text="RUN")
 photo2 = ImageTk.PhotoImage(Image.open("run.png"))
-run.config(image=photo2,width ="160",height = "130", activebackground="black",bg="black", command = callback)
+run.config(image=photo2,width ="160",height = "130", activebackground="black",bg="black", command = execute)
 
-dataset.place(x = 700, y = 590)
+dataset.place(x = 700, y = 585)
 algo.place(x = 400, y = 600)
-run.place(x = 1100, y = 545 )
+run.place(x = 1100, y = 535 )
 
 root.mainloop()
